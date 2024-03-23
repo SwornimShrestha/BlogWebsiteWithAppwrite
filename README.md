8d9c5c225c4633101062f1ed48f330c8e2d9b9fa
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-03-23 10:04:53 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+npm install @reduxjs/toolkit react-redux react-router-dom appwrite @tinymce/tinymce-react html-react-parser react-hook-form
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Blog Website</h1>
+    </>
+  );
+}
+
+export default App;
